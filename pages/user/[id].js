d60345c4ab924a26f8da6fe24e936028a0e722ee
nurcanNT <nrcntpkra@gmail.com>
2023-04-27 01:15:01 +0300

    getServerSideProps Example

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,33 +1,44 @@
 import { useEffect, useState } from "react";
 import {useRouter} from "next/router";
 
-const UserItem = () => {
-    const [user, setUser] = useState();
-    const router = useRouter();
+const UserItem = ({userData}) => {
+    // const [user, setUser] = useState();
+    // const router = useRouter();
 
-    const {id} = router.query;
+    // const {id} = router.query;
 
-    useEffect(()=>{
-        const getData = async ()=>{
-            try{
-             const res = await fetch("https://jsonplaceholder.typicode.com/users");
-             const data = await res.json();
-             setUser(data && data.find((u) => u.id === parseInt(id) ));
-            }catch(err){
-                console.log(err);
-            }
-        };
-        getData(); 
-    }, [id]);
-    console.log(user);
+    // useEffect(()=>{
+    //     const getData = async ()=>{
+    //         try{
+    //          const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    //          const data = await res.json();
+    //          setUser(userData && userData.find((u) => u.id === parseInt(id) ));
+    //         }catch(err){
+    //             console.log(err);
+    //         }
+    //     };
+    //     getData(); 
+    // }, [id, userData]);
+    // console.log(user);
   return (
     <div className='container'>
       <h1>User Item</h1>
       <p>
-        <b>Full Name:</b> {user?.name}
+        <b>Full Name:</b> {userData?.name}
       </p>
     </div>
   )
 }
 
 export default UserItem;
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${context.params.id}`);
+    const userData = await res.json();
+    console.log(context);
+    return{
+        props:{
+            userData,
+        }
+    }
+}
